fbf8ede634cdd113c422a6a6071004fafe11ac9c
CJACOBSON32 <34342644+CJACOBSON32@users.noreply.github.com>
2020-07-31 14:49:01 -0400

    Switched to Hash Router

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import '../css/App.css';
 import theme from './AppStyles.js';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import ThemeProvider from "@material-ui/styles/ThemeProvider";
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {BrowserRouter as HashRouter, Switch, Route, Redirect} from 'react-router-dom';
 import Home from "./pages/Home";
 
 
@@ -11,13 +11,13 @@ function App() {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Router>
+            <HashRouter>
                 <Switch>
                     <Redirect exact from="/" to="/Cameron_Jacobson_Portfolio/Resume" />
-                    <Redirect exact from="/Cameron_Jacobson_Portfolio" to="/Cameron_Jacobson_Portfolio/Resume" />
+                    <Redirect exact from="/Cameron_Jacobson_Portfolio/" to="/Cameron_Jacobson_Portfolio/Resume" />
                     <Route exact path="/Cameron_Jacobson_Portfolio/:page?" render={props => <Home {...props}/>}/>
                 </Switch>
-            </Router>
+            </HashRouter>
         </ThemeProvider>
 
     );
