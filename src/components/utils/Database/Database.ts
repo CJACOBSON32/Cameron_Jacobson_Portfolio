18b035ac2beb49e35475222ed4bc84887e29be1c
CJACOBSON32 <34342644+CJACOBSON32@users.noreply.github.com>
2021-01-19 01:35:20 -0500

    Database working. Tested by logging clicks of 'Enter'.

---
+++ src/components/utils/Database/Database.ts
@@ -0,0 +1,9 @@
+static const firebaseConfig = {
+    apiKey: "AIzaSyAjnHyL8h7frEcC2w5tYXawA1XGYAgUIFg",
+    authDomain: "cameron-jacobson-portfolio.firebaseapp.com",
+    projectId: "cameron-jacobson-portfolio",
+    storageBucket: "cameron-jacobson-portfolio.appspot.com",
+    messagingSenderId: "20381987579",
+    appId: "1:20381987579:web:1a4899628585d6e199365e",
+    measurementId: "G-VMZPFWGEHT"
+};
